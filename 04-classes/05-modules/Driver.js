7fbc4be95f05f0ff357fb9a873dd9f10b38bcc48
Shupermokey <a.e.sobieraj@gmail.com>
2023-07-03 11:39:49 -0500

    Using Modules to extract functinality from a class

---
+++ 04-classes/05-modules/Driver.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const Customer_1 = require("./Customer");
+let myCustomersFive = new Customer_1.CustomerFive("AlexFive", "SobierajFive");
+console.log(myCustomersFive.firstName);
+console.log(myCustomersFive.lastName);
---
+++ 04-classes/05-modules/Driver.ts
@@ -0,0 +1,7 @@
+import { CustomerFive } from "./Customer";
+
+let myCustomersFive = new CustomerFive("AlexFive", "SobierajFive");
+
+
+console.log(myCustomersFive.firstName);
+console.log(myCustomersFive.lastName);
